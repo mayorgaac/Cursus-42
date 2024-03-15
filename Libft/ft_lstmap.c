e292ec798df725a94d5f74b6e0c6f0ca9a7f8bd7
Alvaro Mayorga <amayorga@c2r18s5.42madrid.com>
2024-03-15 23:00:25 +0100

    bonus libft

---
+++ Libft/ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amayorga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 22:53:27 by amayorga          #+#    #+#             */
+/*   Updated: 2024/03/15 22:53:33 by amayorga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*res;
+	t_list	*tmp;
+
+	if (!lst || !f)
+		return (NULL);
+	res = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew((*f)(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, tmp);
+		tmp = tmp->next;
+		lst = lst->next;
+	}
+	return (res);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amayorga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 22:55:58 by amayorga          #+#    #+#             */
+/*   Updated: 2024/03/15 22:56:03 by amayorga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -110,5 +122,38 @@ void				ft_putendl_fd(char *s, int fd);
 /* ft_putnbr_fd */
 void				ft_putnbr_fd(int n, int fd);
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+/* ft_lstnew */
+t_list				*ft_lstnew(void *content);
+
+/* ft_lstadd_front */
+void				ft_lstadd_front(t_list **lst, t_list *new);
+
+/* ft_lstsize */
+int					ft_lstsize(t_list *lst);
+
+/* ft_lstlast */
+t_list				*ft_lstlast(t_list *lst);
+
+/* ft_lsadd_back */
+void				ft_lstadd_back(t_list **lst, t_list *new);
+
+/* ft_lstdelone */
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+
+/* ft_lstclear */
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+
+/* ft_lstiter */
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+
+/*ft_lstmap */
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 #endif
