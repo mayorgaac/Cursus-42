6aed61dadd05cf911b7b15068d14adff6b96e830
Alvaro Mayorga <amayorga@c2r13s5.42madrid.com>
2024-03-18 21:12:05 +0100

    correcciones

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -17,8 +17,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*ptr;
 
-	if (!s)
-		return (NULL);
+	if (!s || ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
 	ptr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!ptr)
 		return (NULL);
